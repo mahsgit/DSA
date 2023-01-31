9d4784ca1cecbeb157c5f0f000034619c0773ca4
Merbebt <103383720+mahsgit@users.noreply.github.com>
2023-01-31 07:36:48 +0300

    Update Quick_sort.cpp

--- Quick_sort.cpp
+++ Quick_sort.cpp
@@ -1,51 +1,42 @@
 #include<iostream>
 using namespace std;
-#define max_size 100
-void inertpolaition(int arr[],int n,int search)
-{
-
-int low=0;
-int high=n-1;
-bool flag=true;
-
-while(low<=high&& search>=arr[low]&&search<=arr[high])
-
-{
-    if (low==high){
-        cout<<"found on index"<<low<<endl;
-
-    }
-    int k=low+(high-low) * ((search -arr[low])/(arr[high]-arr[low]));
-    if(arr[k]==search){
-       cout<<"found on index"<<k<<endl;
-       flag=false;
-    }
-    if(arr[k]<search){
-       low=k+1; 
-    }
-    else{
-	
-    high=k-1;
-    }  
+int patrition(int arr[], int start,int end){
+int pivot=arr[start];
+int count=0;
+for(int i=start+1;i<end;i++){
+    if(arr[i]<pivot){
+        count++;   
 }
-if(flag==true){
-
-cout<<"not found\n";
+int pi=start+count;
+swap(arr[start],arr[pi]);
+int i=start;
+int j=end;
+while(start<end && i<pi && j >pi){
+ if (arr[i]<pivot){
+    i++;
 }
+else if (arr[j]>pivot){
+    j--;
+
+}
+
+}
+if(start<end && i<pi && j >pi ){
+    swap(arr[i++],arr[j--]);
+}
+return pi;
+}
+quick(int arr[],int start,int end){
+    int p= patrition(arr[],start, end);
+    quick(arr,start,p-1);
+    quick(aee,p+1,end);
+
+
 }
 int main(){
-    int n;
-    int arr[max_size];
-    cout<<"enter the size\n";
-    cin>>n;
-    cout<<"enter the number\n";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-
-    }
-    cout<<"enter the number to search\n";
-    int search;
-    cin>>search;
-    inertpolaition(arr,n,search);
+    int arr[]={5,2,9,56,1,9};
+    int end=6;
+    int start=0;
+    quick(arr,start,size-1);
     return 0;
 }
